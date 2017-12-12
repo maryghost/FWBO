3d626f6d05de7d0ea0ca976270faa7a12ba0b70e
Michael Henson <mhenson@gmail.com>
2017-12-11 21:50:52 -0600

    Miguel Torres
    Made arrow buttons smaller and reduced space between names on credits page.

--- wwwroot/javascript/play_flying.js
+++ wwwroot/javascript/play_flying.js
@@ -138,7 +138,7 @@ function gameStart()
 		this.imgRight = new Image();
 		this.imgRight.src = "images/play_flying/arrow-right.png";
 
-		var size = 800 / 8;
+		var size = 800 / 10;
 		var center = 800 / 2;
 		var spacing = 800 / 4;
 		var y = 480 - size - 10;
@@ -289,10 +289,7 @@ function gameStart()
 			this.storeFiles[i++] = PATH_SHOP + "SHOP_GetawaySpa_T1_B12.png";
 			this.storeFiles[i++] = PATH_SHOP + "SHOP_GetawaySpa_T3_H1.png";
 			this.storeFiles[i++] = PATH_SHOP + "SHOP_GetawaySpa_T3_H6.png";
-<<<<<<< HEAD
 			this.storeFiles[i++] = PATH_SHOP + "SHOP_HeadphoneHub_T2_MainHall.png";
-=======
->>>>>>> 45f207bdaf3397298032a28ec6884c0e76d47e1f
 			this.storeFiles[i++] = PATH_SHOP + "SHOP_Hoypoloi_T1_B6.png";
 			this.storeFiles[i++] = PATH_SHOP + "SHOP_Hoypoloi_T2_E5.png";
 			this.storeFiles[i++] = PATH_SHOP + "SHOP_Hudson_T1_B10.png";
@@ -332,11 +329,6 @@ function gameStart()
 			this.storeFiles[i++] = PATH_SHOP + "SHOP_Talie_T3_H1.png";
 			this.storeFiles[i++] = PATH_SHOP + "SHOP_Vosges_T1_B6.png";
 			this.storeFiles[i++] = PATH_SHOP + "SHOP_Vosges_T3_H4.png";
-<<<<<<< HEAD
-=======
-			
-			// TODO: Finish loading storefronts.
->>>>>>> 45f207bdaf3397298032a28ec6884c0e76d47e1f
 			
 			for (var i = 0; i < this.storeFiles.length; i++)
 			{
